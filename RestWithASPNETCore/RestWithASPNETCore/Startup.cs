ef63c4c42c1c301820fa6d0786f1e7972c79e4d8
Marcelo Correa dos Santos <ma_csantos@hotmail.com>
2020-08-01 16:36:04 -0300

    Finaly section 7

--- RestWithASPNETCore/RestWithASPNETCore/Startup.cs
+++ RestWithASPNETCore/RestWithASPNETCore/Startup.cs
@@ -8,6 +8,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using RestWithASPNETCore.Services;
+using RestWithASPNETCore.Services.Implementations;
 
 namespace RestWithASPNETCore
 {
@@ -23,7 +25,11 @@ namespace RestWithASPNETCore
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            //Add Framework services.
             services.AddMvc();
+
+            //Dependency Injection
+            services.AddScoped<IPersonService, PersonServiceImpl>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
